b84f403a314b91b17456cebb1ad6931ecd3784e8
TyrionWang <1102415806@qq.com>
2022-07-25 21:02:06 +0800

    delete GUI

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import airtest.core.android.android
+import logging
 
 from connect import *
 from core import *
@@ -10,9 +11,11 @@ class AndroidDevice(airtest.core.android.android.Android):
         self.dev = connect_device(device_type)
 
 
+logger = logging.getLogger("airtest")
+logger.setLevel(logging.ERROR)
+
 if __name__ == "__main__":
-    global current_device
-    cbox = ['安卓', 'UU模拟器', '蓝叠模拟器']
-    device_type = cbox[0]
+    device_box = ['安卓', 'UU模拟器', '蓝叠模拟器']
+    device_type = device_box[0]
     current_device = AndroidDevice(device_type)
     auto_recruit(current_device.dev)
